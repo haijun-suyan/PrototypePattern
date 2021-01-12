22fd65bd892d37c9fdd09c4ec45a8fa672bd419a
yt_yanhaijun <C2019275@pccc.bankcomm.com>
2021-01-12 20:46:11 +0800

    原型模式设计

---
+++ PrototypePattern/ProtoypePattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProtoypePattern
+//
+//  Created by haijunyan on 2019/2/27.
+//  Copyright © 2019年 haijunyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
